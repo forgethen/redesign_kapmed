da23ce3f181948b67b2c1dfd8ffaa60c2da41c21
forgethen <you@example.com>
2020-08-31 14:34:18 +0300

    update

--- src/js/vanilla.js
+++ src/js/vanilla.js
@@ -25,7 +25,34 @@ const switchshowMore = (elm) => {
   elm.closest('.show_more').classList.toggle('show_true')
 }
 
+const checkReady = () => {
+  const body = document.querySelector('body')
+  let interval
+  interval = setInterval(() => {
+    if (document.readyState === 'complete') {
+      clearInterval(interval)
+      body.removeAttribute('data-preloader')
+      let preloader = document.querySelector('.preloader')
+      setTimeout(() => {
+        preloader.remove()
+      }, 500);
+    }
+  }, 100);
+}
+
 const payload = () => {
+
+  /* preloader */
+
+  const body = document.querySelector('body')
+  if (body.hasAttribute('data-preloader')) {
+    let node = document.createElement('div')
+    node.classList.add('preloader')
+    node.innerHTML = '<div class="blue"><img src="src/img/static/blue.svg"></div><img class="red" src="src/img/static/red.svg"><div class="name"><img src="src/img/static/text.svg"></div>'
+    body.appendChild(node)
+    setTimeout(checkReady, 3200)
+  }
+
   const action_buttons = document.querySelectorAll('.activity-action')
   if (action_buttons.length) openActivityHandler(action_buttons)
 }
